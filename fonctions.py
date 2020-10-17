f39f4be5a1b778c609331ab46344eace12a3e268
Clement Forget <clement.forget@viala-lacoste.com>
2020-10-17 10:08:42 +0000

    Initial commit

---
+++ fonctions.py
@@ -0,0 +1,3 @@
+def add_infos(nom, prenom, classe, age):
+  dic = {'nom': nom, 'prenom': prenom, 'classe': classe, 'age': age}
+  return dic
---
+++ main.py
@@ -0,0 +1,7 @@
+#Imports
+import fonctions
+#Déclaration Var
+classe = []
+
+#programme
+classe.append(fonctions.add_infos(input("Prenom : "), input("Nom : "), input("Classe :"), input("Age : ")))
