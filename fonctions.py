5833824a69b80453d4539abc3abcd957ef398b75
Clement Forget <clement.forget@viala-lacoste.com>
2020-10-17 11:22:01 +0000

    laster

--- fonctions.py
+++ fonctions.py
@@ -2,3 +2,13 @@ def add_infos(nom, prenom, classe, age):
   dic = {'nom': nom, 'prenom': prenom, 'classe': classe, 'age': age}
   return dic
 
+
+def aff_data(classe):
+  n = 1
+  for dico in classe:
+    print(f"{n} - {dico.get('prenom')} {dico.get('nom')}, agé de : {dico.get('age')}, en classe de : {dico.get('classe')}")
+    n = n+1
+
+
+def del_data(classe, num):
+  classe.del[num]
--- main.py
+++ main.py
@@ -2,27 +2,56 @@
 import fonctions
 #Déclaration Var
 classe = []
+finish = False
 
 #programme
 print("Bonjours, ")
 print("Vous êtes dans le programmes de gestions dico")
 print("Pour le moment, votre base de donner ne contient personne")
-print("Pour ajouter des gens faites 1")
 
-menu_ok = False
-while(menu_ok != True):
-  try:
-    menu = int(input("Quelle option choisez vous ? "))
-  except:
-    print("Vous n'avez pas choisi un nombre !")
-    continue
-  if(menu == 1):
-    menu_ok = True
+while(finish != True):
+  menu_ok = False
+  while(menu_ok != True):
+    
+    print(f"Vous avez pour le moment {len(classe)} entrée dans votre table ")
     print("\n")
-    print("Bienvenur dans l'assistant d'ajout")
-    classe.append(fonctions.add_infos(input("Choisisez un Prenom : "), input("Choisisez un Nom : "), input("Choisisez une Classe :"), input("Choisisez un Age : ")))
+    print("Pour ajouter des gens faites 1")
+    print("Pour afficher vos données faites 2")
+    try:
+      menu = int(input("Quelle option choisez vous ? "))
+    except:
+      print("Vous n'avez pas choisi un nombre !")
+      continue
+    if(menu == 1):
+    #Debut ajout
+      menu_ok = True
+      print("\n")
+      print("Bienvenur dans l'assistant d'ajout")
+      classe.append(fonctions.add_infos(input("Choisisez un Prenom : "), input("Choisisez un Nom : "), input("Choisisez une Classe :"), input("Choisisez un Age : ")))
+  #Affichage table
+      print("\n")
+      print('Vos données : ')
+      print("\n")
+      print(fonctions.aff_data(classe))
+      print("\n")
+    
+    elif(menu == 2):
+  #Affichage table
+      print("\n")
+      print('Vos données : ')
+      print("\n")
+      print(fonctions.aff_data(classe))
+      print("\n")
 
-  else:
-    print("\n")
-    print("Votre entez n'est pas valide !")
-    continue
+    elif(menu == 3):
+      print("\n")
+      print('Vos données : ')
+      print("\n")
+      print(fonctions.aff_data(classe))
+      print("\n")
+      
+
+    else:
+      print("\n")
+      print("Votre entez n'est pas valide !")
+      continue
