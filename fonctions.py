a023344f4ef50fff11401b6712b18c36cd4c342d
Clement Forget <clement.forget@viala-lacoste.com>
2020-10-17 10:24:24 +0000

    add add

--- fonctions.py
+++ fonctions.py
@@ -1,3 +1,4 @@
 def add_infos(nom, prenom, classe, age):
   dic = {'nom': nom, 'prenom': prenom, 'classe': classe, 'age': age}
   return dic
+
--- main.py
+++ main.py
@@ -4,4 +4,25 @@ import fonctions
 classe = []
 
 #programme
-classe.append(fonctions.add_infos(input("Prenom : "), input("Nom : "), input("Classe :"), input("Age : ")))
+print("Bonjours, ")
+print("Vous êtes dans le programmes de gestions dico")
+print("Pour le moment, votre base de donner ne contient personne")
+print("Pour ajouter des gens faites 1")
+
+menu_ok = False
+while(menu_ok != True):
+  try:
+    menu = int(input("Quelle option choisez vous ? "))
+  except:
+    print("Vous n'avez pas choisi un nombre !")
+    continue
+  if(menu == 1):
+    menu_ok = True
+    print("\n")
+    print("Bienvenur dans l'assistant d'ajout")
+    classe.append(fonctions.add_infos(input("Choisisez un Prenom : "), input("Choisisez un Nom : "), input("Choisisez une Classe :"), input("Choisisez un Age : ")))
+
+  else:
+    print("\n")
+    print("Votre entez n'est pas valide !")
+    continue
